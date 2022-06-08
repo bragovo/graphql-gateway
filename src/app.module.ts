11966a1449cd40fec1d546f6cea244447e993ffe
Andrei Brizhinev <abrizhinyov@samokat.ru>
2022-06-08 18:01:06 +0300

    Add gateway example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        // ... Apollo server options
+        cors: true,
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'catalog', url: 'http://localhost:3001/api/v1/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
 })
 export class AppModule {}
